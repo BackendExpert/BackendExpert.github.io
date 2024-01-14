2051ec265ad13c4565ec7b00cafefb17139bb1a4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-14 19:11:58 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ fetch("https://api.github.com/users/BackendExpert")
                 <h1>Username: ${data.login}</h1><br>
                 <h3>Bio: ${data.bio}</h3>
                 <h3>Public Repositories: ${data.public_repos}</h3>
+                <h3>Followers: ${data.followers}</h3>
             </div>
             <div class='jk'>
             sss
