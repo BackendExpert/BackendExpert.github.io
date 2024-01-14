56fba11de8a8789bbb14a60df0e8854782eb7668
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-14 19:31:37 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app.js
+++ app.js
@@ -20,13 +20,13 @@ fetch("https://api.github.com/users/jehankandy")
   
     var jkData = document.getElementById('gitResultJK');
     let allJK = `
-            <div class='bk'>
-                <h1>Username: ${data.login}</h1><br>
-                <h3>Bio: ${data.bio}</h3>
-                <h3>Public Repositories: ${data.public_repos}</h3>
-                <h3>Followers: ${data.followers}</h3>
+            <div class='jk'>
+                <h1>Username: ${jkdata.login}</h1><br>
+                <h3>Bio: ${jkdata.bio}</h3>
+                <h3>Public Repositories: ${jkdata.public_repos}</h3>
+                <h3>Followers: ${jkdata.followers}</h3>
                 <br><br>
-                <h4>For More Info: <a href='${data.html_url}' target='_blank' style='text-decoration: none; color:rgb(49, 58, 184);'>${data.login}</a></h4>
+                <h4>For More Info: <a href='${jkdata.html_url}' target='_blank' style='text-decoration: none; color:rgb(49, 58, 184);'>${jkdata.login}</a></h4>
             </div>`
     jkData.innerHTML = allJK;
 })
