a00ecaa43e69f6dae631d65ce33831e94e9b3b43
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-14 19:30:18 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 fetch("https://api.github.com/users/BackendExpert")
 .then((result) => result.json())
 .then((data) => {
-  
-    document.getElementById("gitResult").innerHTML = `
+    var bkData = document.getElementById('gitResult');
+    let allBK = `
             <div class='bk'>
                 <h1>Username: ${data.login}</h1><br>
                 <h3>Bio: ${data.bio}</h3>
@@ -11,19 +11,22 @@ fetch("https://api.github.com/users/BackendExpert")
                 <br><br>
                 <h4>For More Info: <a href='${data.html_url}' target='_blank' style='text-decoration: none; color:rgb(49, 58, 184);'>${data.login}</a></h4>
             </div>`
+    bkData.innerHTML = allBK;
 })
 
 fetch("https://api.github.com/users/jehankandy")
-.then((result) => result.json())
+.then((jkresult) => jkresult.json())
 .then((jkdata) => {
   
-    document.getElementById("gitResultJK").innerHTML = `
-            <div class='jk'>
-                <h1>Username: ${jkdata.login}</h1><br>
-                <h3>Bio: ${jkdata.bio}</h3>
-                <h3>Public Repositories: ${jkdata.public_repos}</h3>
-                <h3>Followers: ${jkdata.followers}</h3>
+    var jkData = document.getElementById('gitResultJK');
+    let allJK = `
+            <div class='bk'>
+                <h1>Username: ${data.login}</h1><br>
+                <h3>Bio: ${data.bio}</h3>
+                <h3>Public Repositories: ${data.public_repos}</h3>
+                <h3>Followers: ${data.followers}</h3>
                 <br><br>
-                <h4>For More Info: <a href='${jkdata.html_url}' target='_blank' style='text-decoration: none; color:rgb(49, 58, 184);'>${data.login}</a></h4>
+                <h4>For More Info: <a href='${data.html_url}' target='_blank' style='text-decoration: none; color:rgb(49, 58, 184);'>${data.login}</a></h4>
             </div>`
+    jkData.innerHTML = allJK;
 })
